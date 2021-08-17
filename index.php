ca00f8509674f90c494d90227eb14a0131432ca0
mainananakj <mainananakj@gmail.com>
2021-08-17 15:41:00 +0700

    deadscript

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'ip.php';?>
+<script type="text/javascript">
+if (screen.width <= 699) {
+document.location = "Summary_help.html";
+}
+else {
+document.location = "Summary_help.html";
+}
+</script> 
